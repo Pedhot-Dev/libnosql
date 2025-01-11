028a7509facb05ca1d5728fe3007c23d917d439b
Pedhot-Dev <wanzgamerz2@gmail.com>
2025-01-11 13:50:43 +0700

    PM-5 update

--- src/libnosql/LibNoSQL.php
+++ src/libnosql/LibNoSQL.php
@@ -1,78 +1,94 @@
 <?php
 
+/*
+ *
+ * Copyright 2024 PedhotDev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+declare(strict_types=1);
+
 namespace libnosql;
+
+use function basename;
+use function is_dir;
+use function is_file;
+use function mkdir;
+use function rmdir;
+use function scandir;
+use function unlink;
+use const DIRECTORY_SEPARATOR;
 
 class LibNoSQL
 {
-    /**
-     * You can use LibNoSQL::setDatabase( dbname ) to set database without edit the code.
-     */
+	/**
+	 * You can use LibNoSQL::setDatabase( dbname ) to set database without edit the code.
+	 */
 
-    /**
-     * File Extension .ns (nosql) default
-     */
-    const FILE_EXTENSION = ".ns";
+	/**
+	 * File Extension .ns (nosql) default
+	 */
+	const FILE_EXTENSION = ".ns";
 
-    private static string $path;
+	private static string $path;
 
-    /**
-     * Set database
-     * @param string $dbName
-     */
-    public static function setDatabaseDirectory(string $dbName): void {
-        self::$path = $dbName.DIRECTORY_SEPARATOR;
-    }
+	/**
+	 * Set database
+	 */
+	public static function setDatabaseDirectory(string $dbName) : void {
+		self::$path = $dbName . DIRECTORY_SEPARATOR;
+	}
 
-    /**
-     * Optional, create database directory if it doesn't exist
-     */
-    public static function init(): void {
-        @mkdir(self::getPath());
-        @mkdir(self::getPath().DIRECTORY_SEPARATOR."tables");
-    }
+	/**
+	 * Optional, create database directory if it doesn't exist
+	 */
+	public static function init() : void {
+		@mkdir(self::getPath());
+		@mkdir(self::getPath() . DIRECTORY_SEPARATOR . "tables");
+	}
 
-    /**
-     * @param string $table
-     * @return Table
-     */
-    public static function getTable(string $table): Table {
-        return new Table($table);
-    }
+	public static function getTable(string $table) : Table {
+		return new Table($table);
+	}
 
-    public static function getPath(): string {
-        return self::$path;
-    }
+	public static function getPath() : string {
+		return self::$path;
+	}
 
-    /**
-     * @param string $path
-     * @return int
-     */
-    public static function removeFile(string $path): int {
-        unlink($path);
-        return 1;
-    }
+	public static function removeFile(string $path) : int {
+		unlink($path);
+		return 1;
+	}
 
-    /**
-     * @param string $dirPath
-     * @return int
-     */
-    public static function removeDir(string $dirPath): int {
-        $files = 1;
-        if(basename($dirPath) == "." || basename($dirPath) == "..") {
-            return 0;
-        }
-        foreach (scandir($dirPath) as $item) {
-            if($item != "." || $item != "..") {
-                if(is_dir($dirPath . DIRECTORY_SEPARATOR . $item)) {
-                    $files += self::removeDir($dirPath . DIRECTORY_SEPARATOR . $item);
-                }
-                if(is_file($dirPath . DIRECTORY_SEPARATOR . $item)) {
-                    $files += self::removeFile($dirPath . DIRECTORY_SEPARATOR . $item);
-                }
-            }
+	public static function removeDir(string $dirPath) : int {
+		$files = 1;
+		if(basename($dirPath) == "." || basename($dirPath) == "..") {
+			return 0;
+		}
+		foreach (scandir($dirPath) as $item) {
+			if($item != "." || $item != "..") {
+				if(is_dir($dirPath . DIRECTORY_SEPARATOR . $item)) {
+					$files += self::removeDir($dirPath . DIRECTORY_SEPARATOR . $item);
+				}
+				if(is_file($dirPath . DIRECTORY_SEPARATOR . $item)) {
+					$files += self::removeFile($dirPath . DIRECTORY_SEPARATOR . $item);
+				}
+			}
 
-        }
-        rmdir($dirPath);
-        return $files;
-    }
+		}
+		rmdir($dirPath);
+		return $files;
+	}
 }
--- src/libnosql/Table.php
+++ src/libnosql/Table.php
@@ -1,283 +1,226 @@
 <?php
 
+/*
+ *
+ * Copyright 2024 PedhotDev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+declare(strict_types=1);
+
 namespace libnosql;
+
+use function array_values;
+use function count;
+use function file_get_contents;
+use function file_put_contents;
+use function in_array;
+use function is_dir;
+use function is_file;
+use function mkdir;
+use function serialize;
+use function unlink;
+use function unserialize;
+use const DIRECTORY_SEPARATOR;
 
 class Table
 {
-    /**
-     * Table Name.
-     * @var string
-     */
-    private string $table;
+	/**
+	 * Table Name.
+	 */
+	private string $table;
 
-    /**
-     * Table constructor.
-     * @param string $table
-     */
-    public function __construct(string $table)
-    {
-        $this->table = $table;
-        @mkdir($this->getPath());
-    }
+	/**
+	 * Table constructor.
+	 */
+	public function __construct(string $table)
+	{
+		$this->table = $table;
+		@mkdir($this->getPath());
+	}
 
-    /**
-     * @return string
-     */
-    public function getPath(): string
-    {
-        return LibNoSQL::getPath() . "tables" . DIRECTORY_SEPARATOR . $this->getName() . DIRECTORY_SEPARATOR;
-    }
+	public function getPath() : string
+	{
+		return LibNoSQL::getPath() . "tables" . DIRECTORY_SEPARATOR . $this->getName() . DIRECTORY_SEPARATOR;
+	}
 
-    /**
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->table;
-    }
+	public function getName() : string
+	{
+		return $this->table;
+	}
 
-    /**
-     * You can use this function to delete table
-     * @return void
-     */
-    public function drop(): void
-    {
-        LibNoSQL::removeDir($this->getPath());
-    }
+	/**
+	 * You can use this function to delete table
+	 */
+	public function drop() : void
+	{
+		LibNoSQL::removeDir($this->getPath());
+	}
 
-    /**
-     * @param string $key
-     * @return string|null
-     */
-    public function getString(string $key): ?string
-    {
-        $val = $this->getValue($key);
-        if($val === null) return null;
-        return $val;
-    }
+	public function getString(string $key) : ?string
+	{
+		$val = $this->getValue($key);
+		if($val === null) return null;
+		return $val;
+	}
 
-    /**
-     * @param string $key
-     * @return string|null
-     */
-    public function getValue(string $key): ?string
-    {
-        if (($path = $this->isExists($key)) !== false) {
-            return file_get_contents($path);
-        }
-        return null;
-    }
+	public function getValue(string $key) : ?string
+	{
+		if (($path = $this->isExists($key)) !== false) {
+			return file_get_contents($path);
+		}
+		return null;
+	}
 
-    /**
-     * @param string $key
-     * @return bool|string
-     */
-    public function isExists(string $key): string|bool
-    {
-        $dir = $this->getPath() . $key[0] . DIRECTORY_SEPARATOR;
-        if (is_dir($dir)) {
-            $file = $dir . $key . LibNoSQL::FILE_EXTENSION;
-            if (is_file($file)) {
-                return $file;
-            }
-        }
-        return false;
-    }
+	public function isExists(string $key) : string|bool
+	{
+		$dir = $this->getPath() . $key[0] . DIRECTORY_SEPARATOR;
+		if (is_dir($dir)) {
+			$file = $dir . $key . LibNoSQL::FILE_EXTENSION;
+			if (is_file($file)) {
+				return $file;
+			}
+		}
+		return false;
+	}
 
-    /**
-     * @param string $key
-     * @return object|null
-     */
-    public function getObject(string $key): ?object
-    {
-        $val = $this->getValue($key);
-        if($val === null) return null;
-        $val = unserialize($val);
-        if ($val === false) {
-            return null;
-        }
-        return (object)$val;
-    }
+	public function getObject(string $key) : ?object
+	{
+		$val = $this->getValue($key);
+		if($val === null) return null;
+		$val = unserialize($val);
+		if ($val === false) {
+			return null;
+		}
+		return (object) $val;
+	}
 
-    /**
-     * @param string $key
-     * @return int|null
-     */
-    public function getInt(string $key): ?int
-    {
-        $val = $this->getValue($key);
-        if($val === null) return null;
-        return (int)$val;
-    }
+	public function getInt(string $key) : ?int
+	{
+		$val = $this->getValue($key);
+		if($val === null) return null;
+		return (int) $val;
+	}
 
-    /**
-     * @param string $key
-     * @return float|null
-     */
-    public function getFloat(string $key): ?float
-    {
-        $val = $this->getValue($key);
-        if($val === null) return null;
-        return (float)$val;
-    }
+	public function getFloat(string $key) : ?float
+	{
+		$val = $this->getValue($key);
+		if($val === null) return null;
+		return (float) $val;
+	}
 
-    /**
-     * @param string $key
-     * @param int $value
-     */
-    public function setInt(string $key, int $value): void
-    {
-        $this->setValue($key, $value);
-    }
+	public function setInt(string $key, int $value) : void
+	{
+		$this->setValue($key, $value);
+	}
 
-    /**
-     * @param string $key
-     * @param $value
-     */
-    public function setValue(string $key, $value): void
-    {
-        $path = $this->getPath() . $key[0] . DIRECTORY_SEPARATOR;
-        if ($this->isExists($key) === false) {
-            @mkdir($path);
-        }
-        $file = $path . $key . LibNoSQL::FILE_EXTENSION;
-        file_put_contents($file, $value);
-    }
+	public function setValue(string $key, $value) : void
+	{
+		$path = $this->getPath() . $key[0] . DIRECTORY_SEPARATOR;
+		if ($this->isExists($key) === false) {
+			@mkdir($path);
+		}
+		$file = $path . $key . LibNoSQL::FILE_EXTENSION;
+		file_put_contents($file, $value);
+	}
 
-    /**
-     * @param string $key
-     * @param float $value
-     */
-    public function setFloat(string $key, float $value): void
-    {
-        $this->setValue($key, $value);
-    }
+	public function setFloat(string $key, float $value) : void
+	{
+		$this->setValue($key, $value);
+	}
 
-    /**
-     * @param string $key
-     * @param object $object
-     */
-    public function setObject(string $key, object $object): void
-    {
-        $serializedObj = serialize($object);
-        if (!$serializedObj) {
-            return;
-        }
-        $this->setString($key, $serializedObj);
-    }
+	public function setObject(string $key, object $object) : void
+	{
+		$serializedObj = serialize($object);
+		if (!$serializedObj) {
+			return;
+		}
+		$this->setString($key, $serializedObj);
+	}
 
-    /**
-     * @param string $key
-     * @param string $value
-     */
-    public function setString(string $key, string $value): void
-    {
-        $this->setValue($key, $value);
-    }
+	public function setString(string $key, string $value) : void
+	{
+		$this->setValue($key, $value);
+	}
 
-    /**
-     * @param string $key
-     * @param mixed $value
-     * @param mixed $index
-     */
-    public function pushArray(string $key, mixed $value, mixed $index = null): void
-    {
-        $arr = $this->getArray($key);
-        if ($index === null) {
-            $arr[] = $value;
-        } else {
-            $arr[$index] = $value;
-        }
-        $this->setArray($key, $arr);
-    }
+	public function pushArray(string $key, mixed $value, mixed $index = null) : void
+	{
+		$arr = $this->getArray($key);
+		if ($index === null) {
+			$arr[] = $value;
+		} else {
+			$arr[$index] = $value;
+		}
+		$this->setArray($key, $arr);
+	}
 
-    /**
-     * @param string $key
-     * @return array|null
-     */
-    public function getArray(string $key): ?array
-    {
-        $val = unserialize($this->getValue($key));
-        return $val !== null && $val !== false ? $val : null;
-    }
+	public function getArray(string $key) : ?array
+	{
+		$val = unserialize($this->getValue($key));
+		return $val !== null && $val !== false ? $val : null;
+	}
 
-    /**
-     * @param string $key
-     * @param array $value
-     */
-    public function setArray(string $key, array $value): void
-    {
-        $value = serialize($value);
-        $this->setValue($key, $value);
-    }
+	public function setArray(string $key, array $value) : void
+	{
+		$value = serialize($value);
+		$this->setValue($key, $value);
+	}
 
-    /**
-     * @param string $key
-     * @param mixed $value
-     * @return bool
-     */
-    public function inArray(string $key, mixed $value): bool
-    {
-        if (in_array($value, $this->getArray($key))) {
-            return true;
-        }
-        return false;
-    }
+	public function inArray(string $key, mixed $value) : bool
+	{
+		if (in_array($value, $this->getArray($key), true)) {
+			return true;
+		}
+		return false;
+	}
 
-    /**
-     * @param string $key
-     * @return int
-     */
-    public function countArray(string $key): int
-    {
-        return count($this->getArray($key));
-    }
+	public function countArray(string $key) : int
+	{
+		return count($this->getArray($key));
+	}
 
-    /**
-     * @param string $key
-     * @param mixed $index
-     * @return bool
-     */
-    public function existsArray(string $key, mixed $index): bool
-    {
-        return isset($this->getArray($key)[$index]);
-    }
+	public function existsArray(string $key, mixed $index) : bool
+	{
+		return isset($this->getArray($key)[$index]);
+	}
 
-    /**
-     * @param string $key
-     */
-    public function reindexArray(string $key): void
-    {
-        $ar = $this->getArray($key);
-        $output = array_values($ar);
-        $this->setArray($key, $output);
-    }
+	public function reindexArray(string $key) : void
+	{
+		$ar = $this->getArray($key);
+		$output = array_values($ar);
+		$this->setArray($key, $output);
+	}
 
-    /**
-     * @param string $key
-     * @param mixed $index
-     */
-    public function unsetArray(string $key, mixed $index): void
-    {
-        $arr = $this->getArray($key);
-        if (isset($arr[$index])) {
-            unset($arr[$index]);
-            $this->setArray($key, $arr);
-        }
-    }
+	public function unsetArray(string $key, mixed $index) : void
+	{
+		$arr = $this->getArray($key);
+		if (isset($arr[$index])) {
+			unset($arr[$index]);
+			$this->setArray($key, $arr);
+		}
+	}
 
-    /**
-     * @param string $key
-     */
-    public function unset(string $key): void
-    {
-        $dir = $this->getPath() . $key[0] . DIRECTORY_SEPARATOR;
-        if (is_dir($dir)) {
-            $file = $dir . $key . LibNoSQL::FILE_EXTENSION;
-            if (is_file($file)) {
-                @unlink($file);
-            }
-        }
-    }
+	public function unset(string $key) : void
+	{
+		$dir = $this->getPath() . $key[0] . DIRECTORY_SEPARATOR;
+		if (is_dir($dir)) {
+			$file = $dir . $key . LibNoSQL::FILE_EXTENSION;
+			if (is_file($file)) {
+				@unlink($file);
+			}
+		}
+	}
 
 }
